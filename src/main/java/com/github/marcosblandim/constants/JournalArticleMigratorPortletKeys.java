a41c86d3ab12bbbad24f99d9b73f1a04a603ee2a
marcos.blandim <marcos.blandim@seatecnologia.com.br>
2021-09-03 11:13:58 -0300

    feat: empty mvc-portlet

---
+++ src/main/java/com/github/marcosblandim/constants/JournalArticleMigratorPortletKeys.java
@@ -0,0 +1,11 @@
+package com.github.marcosblandim.constants;
+
+/**
+ * @author marco
+ */
+public class JournalArticleMigratorPortletKeys {
+
+	public static final String JOURNALARTICLEMIGRATOR =
+		"com_github_marcosblandim_JournalArticleMigratorPortlet";
+
+}
